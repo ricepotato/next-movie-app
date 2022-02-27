e6f4976534a1cfa5747a5d8d730c47a11aa0a44e
ricepotato <sukjun40@naver.com>
2022-02-27 16:07:00 +0900

    feat: server side api add rewrites

--- next.config.js
+++ next.config.js
@@ -1,6 +1,51 @@
 /** @type {import('next').NextConfig} */
+const API_KEY = process.env.API_KEY;
+const baseUrl = "https://api.themoviedb.org/3/";
 const nextConfig = {
   reactStrictMode: true,
+  async redirects() {
+    return [];
+  },
+  async rewrites() {
+    return [
+      {
+        source: "/api/tv/top_rated",
+        destination: `${baseUrl}tv/top_rated?api_key=${API_KEY}`,
+      },
+      {
+        source: "/api/tv/popular",
+        destination: `${baseUrl}tv/popular?api_key=${API_KEY}`,
+      },
+      {
+        source: "/api/tv/airing_today",
+        destination: `${baseUrl}tv/airing_today?api_key=${API_KEY}`,
+      },
+      {
+        source: "/api/tv/:id",
+        destination: `${baseUrl}tv/:id?api_key=${API_KEY}`,
+      },
+      {
+        source: "/api/movies",
+        destination: `${baseUrl}movie/popular?api_key=${API_KEY}`,
+      },
+      {
+        source: "/api/movies/:id",
+        destination: `${baseUrl}movie/:id?api_key=${API_KEY}`,
+      },
+      {
+        source: "/api/movies/now_playing",
+        destination: `${baseUrl}movie/now_playing?api_key=${API_KEY}`,
+      },
+      {
+        source: "/api/movies/upcoming",
+        destination: `${baseUrl}movie/upcoming?api_key=${API_KEY}`,
+      },
+      {
+        source: "/api/movies/popular",
+        destination: `${baseUrl}movie/popular?api_key=${API_KEY}`,
+      },
+    ];
+  },
 }
 
 module.exports = nextConfig
