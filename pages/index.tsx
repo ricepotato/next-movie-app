6978407f8c6805c4229d003b1d8ad4a12762df9c
ricepotato <sukjun40@naver.com>
2022-02-28 01:46:36 +0900

    feat: home index page

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,9 +3,9 @@ import Seo from "../components/Seo";
 
 const Home: NextPage = () => {
   return (
-    <div>
+    <div className="pt-16 px-5 bg-slate-800 h-screen text-white">
       <Seo title="Next Movie App"></Seo>
-      <h1 className="text-red-400">Hello Next Movie App</h1>
+      <h1 className="text-2xl font-semibold">Hello! Next Movie App 😀</h1>
     </div>
   );
 };
