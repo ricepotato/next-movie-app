00b32a58a3a0b35001c1fed087764eeb9fd023a9
ricepotato <sukjun40@naver.com>
2022-02-28 00:10:51 +0900

    feat: search page

--- pages/search/index.tsx
+++ pages/search/index.tsx
@@ -3,9 +3,10 @@ import Seo from "../../components/Seo";
 
 const Search: NextPage = () => {
   return (
-    <div>
+    <div className="pt-16 px-5 bg-slate-800 h-screen text-white">
       <Seo title="Search"></Seo>
-      <h1>Search</h1>
+      <h1 className="py-2 font-semibold text-2xl ">Search</h1>
+      <div className="text-lg"> Work in progress... 😅</div>
     </div>
   );
 };
