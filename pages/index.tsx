7603e924d7def49870e5fec1047c5fb1b8ff8242
ricepotato <sukjun40@naver.com>
2022-02-27 19:46:48 +0900

    feat: install tailwindcss

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,13 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
-import Seo from "../components/Seo"
+import type { NextPage } from "next";
+import Seo from "../components/Seo";
 
 const Home: NextPage = () => {
   return (
-    <div className={styles.container}>
+    <div>
       <Seo title="Next Movie App"></Seo>
-      <h1>Hello movie app</h1>
+      <h1 className="text-red-400">Hello Next Movie App</h1>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  content: [
+    "./pages/**/*.{js,jsx,ts,tsx}",
+    "./components/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  darkMode: "class", // 컴퓨터 설정에 따름 'media', class 설정에 따름 'class'
+};
